c340c7f547219776438704cf693d54288cb479e0
george-sa <georgesa201097@gmail.com>
2022-02-09 09:57:50 -0300

    Implementação do app de cadastro

---
+++ App de cadastro/AppCadastro/Classes/SerieRepositorio.cs
@@ -0,0 +1,38 @@
+using AppCadastro.Series.interfaces;
+
+namespace AppCadastro.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;  //o Id é o próprio índice do vetor
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
---
+++ App de cadastro/AppCadastro/Enum/Genero.cs
@@ -0,0 +1,29 @@
+namespace AppCadastro.Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+
+        Aventura = 2,
+
+        Comedia = 4,
+
+        Drama = 5,
+
+        Espionagem = 6,
+
+        Faroeste = 7,
+
+        Fantasia = 8,
+
+        Ficcao_Cientifica = 9,
+
+        Musical = 10, 
+
+        Romance = 11,
+
+        Suspense = 12,
+
+        Terror = 13
+    }
+}
